6aa9f7b4ff87ad3b1b7fc4e4d631979702261741
trumpetyk09 <72859693+trumpetyk09@users.noreply.github.com>
2020-10-14 18:18:31 +0900

    임양규 20201014 0618
    임양규 2020년10월14일 06시18분

---
+++ Duksung_mouse_Answer_2020_10_14_09_15_28/sketch.js
@@ -0,0 +1,49 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  // background(220);
+  cursor(CROSS);
+  // rect(10, 10, 50, 50);
+  // rect(mouseX, mouseY, 50, 50);
+  // rect(10, 10, mouseX, mouseY);
+  // print(mouseX, mouseY);
+}
+
+function mouseMoved(){
+  // print(mouseX, mouseY);
+}
+
+function mouseClicked(){
+  // background(0, 255, 0);
+  print("Click");
+  // print(mouseX, mouseY);
+}
+
+function doubleClicked(){
+  // background(0, 0, 255);
+  print("double Click");
+}
+
+function mousePressed(){
+  // background(0, 0, 0);
+  print("Press");
+  // print(mouseX, mouseY);
+  // rect(mouseX, mouseY, 50, 50);
+}
+
+function mouseReleased(){
+  // background(255, 0, 0);
+  print("Release");
+  // print(mouseX, mouseY);
+}
+
+function mouseDragged(){
+print("Drag");
+  // rect(mouseX, mouseY, 50, 50);
+}
+
+function mouseWheel(){
+ print("wheel");
+}
